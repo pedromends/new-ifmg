36e7af6710f9e4e12d3a1c0974c9905ed7280a57
pedromends <joaopedromend14@gmail.com>
2024-09-25 13:33:55 -0300

    go

--- src/views/Capacitation.vue
+++ src/views/Capacitation.vue
@@ -10,13 +10,30 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="m1 9 4-4-4-4" />
                     </svg>
-                    <span>Editais</span>
+                    <span>Capacitação</span>
                 </p>
             </div>
-            <h1 class="text-4xl text-black font-semibold underline underline-offset-2 decoration-8 decoration-maingreen">Capacitação</h1>
+            <div class="flex justify-between">
+                <h1 class="text-4xl text-black font-semibold underline underline-offset-2 decoration-8 decoration-maingreen">Capacitação</h1>
+                <button v-if="isAdmin"
+                    class="text-white bg-maingreen hover:bg-govblue focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition duration-200"
+                    type="submit" @click.prevent="createNew()">Editar</button>
+            </div>
             <hr class="bg-red-600 h-1"/>
             <div class="flex flex-col gap-5 text-maingray">
-                <!-- Seção p/ divulgar nossas capacitações de RH -->
+                <div class="flex gap-10 max-lg:flex-col">
+                    <div class="w-3/4 flex flex-col gap-10 max-lg:w-auto">
+                        <p>O Polo do Instituto Federal de Minas Gerais (IFMG) se destaca por sua excelência em promover o
+                            desenvolvimento contínuo de habilidades técnicas e profissionais, alinhadas às demandas do mercado. Com uma infraestrutura
+                            moderna e equipada com tecnologias de ponta, o Polo oferece treinamentos especializados nas mais diversas áreas, como inovação
+                            tecnológica, gestão de projetos e sustentabilidade. Além disso, o programa de capacitação é estruturado para atender tanto os
+                            estudantes quanto os profissionais da comunidade local, reforçando o compromisso do IFMG em impulsionar o crescimento educacional
+                            e econômico da região. Por meio de oficinas práticas, palestras com especialistas renomados e cursos certificados, o Polo se
+                            consolida como um centro de referência para aqueles que buscam aperfeiçoamento e atualização nas suas respectivas áreas de atuação.
+                        </p>
+                    </div>
+                </div>
+                <BackToTop />
             </div>
         </div>
         <BackToTop />
@@ -30,6 +47,18 @@ export default {
     name: 'CapacitationPage',
     components: {
         BackToTop
+    },
+    data() {
+        return {
+            news: null,
+            isAdmin: false,
+            page: 0,
+            totalPages: 0,
+            totalElements: 0
+        }
+    },
+    created() {
+        this.isAdmin = this.$store.getters.isAdmin
     },
     props: {
     }
